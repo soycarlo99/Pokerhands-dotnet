eb28bc914fa080b6bd12d9df49e4ea113df9ee1d
Ironboy <ironboy@users.noreply.github.com>
2024-04-03 11:48:52 +0200

    Just moved a Console.Write slightly

--- Program.cs
+++ Program.cs
@@ -3,42 +3,43 @@
 using System;
 using CardGame;
 
-    class Program
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+
+        while (true)
         {
+            // Create a new deck of cards
+            DeckOfCards deck = new DeckOfCards();
+            Console.WriteLine("Deck created and shuffled.");
 
-            while (true)
+            // Deal two hands
+            Hand hand1 = deck.DealHand();
+            Hand hand2 = deck.DealHand();
+            Console.WriteLine("Hands dealt.");
+            Console.WriteLine("Hand 1: " + hand1);
+            Console.WriteLine("Hand 2: " + hand2);
+
+            // Compare hands to determine the winner
+            var winner = CompareHands.CheckHands(hand1, hand2);
+
+            if (winner.winningHand == hand1)
             {
-                // Create a new deck of cards
-                DeckOfCards deck = new DeckOfCards();
-                Console.WriteLine("Deck created and shuffled.");
-
-                // Deal two hands
-                Hand hand1 = deck.DealHand();
-                Hand hand2 = deck.DealHand();
-                Console.WriteLine("Hands dealt.");
-
-                // Compare hands to determine the winner
-                var winner = CompareHands.CheckHands(hand1, hand2);
-                Console.WriteLine("Hand 1: " + hand1);
-                Console.WriteLine("Hand 2: " + hand2);
-                if (winner.winningHand == hand1)
-                {
-                    Console.WriteLine("Hand 1 wins!");
-                }
-                else if (winner.winningHand == hand2)
-                {
-                    Console.WriteLine("Hand 2 wins!");
-                }
-                else
-                {
-                    Console.WriteLine("It's a tie!");
-                }
-                //Console.WriteLine("Winner: " + winner.winningHand);
-                Console.WriteLine("Hand Type: " + winner.handType);
-                //Console.WriteLine(CompareHands.CheckHands(hand1, hand2));
-                //Console.WriteLine(winner == hand1 ? "Hand 1 wins!" : winner == hand2 ? "Hand 2 wins!" : "It's a tie!");
+                Console.WriteLine("Hand 1 wins!");
+            }
+            else if (winner.winningHand == hand2)
+            {
+                Console.WriteLine("Hand 2 wins!");
+            }
+            else
+            {
+                Console.WriteLine("It's a tie!");
+            }
+            //Console.WriteLine("Winner: " + winner.winningHand);
+            Console.WriteLine("Hand Type: " + winner.handType);
+            //Console.WriteLine(CompareHands.CheckHands(hand1, hand2));
+            //Console.WriteLine(winner == hand1 ? "Hand 1 wins!" : winner == hand2 ? "Hand 2 wins!" : "It's a tie!");
 
 
             // Print the result
@@ -54,15 +55,11 @@ using CardGame;
                 Console.WriteLine(winner);
             }*/
             Console.WriteLine("Press Enter to play again, or any other key to exit.");
-                if (Console.ReadKey().Key != ConsoleKey.Enter)
-                {
-                    Console.WriteLine("Thanks for playing!");
-                    break;
-                }
+            if (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+                Console.WriteLine("Thanks for playing!");
+                break;
             }
         }
     }
-
-
-
-
+}
