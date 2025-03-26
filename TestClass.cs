e0181c99ee214417b75ee5c6069494784c438a79
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2025-03-26 15:57:39 +0100

    .

--- TestClass.cs
+++ TestClass.cs
@@ -70,4 +70,30 @@ public class TestClass
         Assert.Equal("Full House", handType);
     }
 
+    [Fact]
+    public void TestMethodIsPair()
+    {
+        // Arrange
+        var pairHand = new Hand(
+        [
+            new Card('♣', '5'),
+            new Card('♠', '5'),
+            new Card('♠', '4'),
+            new Card('♣', '7'),
+            new Card('♠', '9')
+        ]);
+
+
+        // Act
+
+        var (hand1Result, handType) = CompareHands.IsPair(pairHand);
+
+        // Assert
+        // Example of types of asserts
+        Assert.NotNull(hand1Result);
+        Assert.NotNull(handType);
+        Assert.True(hand1Result == pairHand);
+        Assert.Equal("Pair", handType);
+    }
+
 }
